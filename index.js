d54388d4cc05177739547c45acf06d6f92a545a9
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2023-02-14 00:25:41 +0600

    user update api got

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ app.post("/projects", projectRoute);
 app.get("/collections", projectRoute);
 app.get("/users", userRoute);
 app.get("/user", userRoute);
-app.put("/u/:email", userRoute);
+app.get("/u", userRoute);
+app.get("/u/:id", userRoute);
+app.put("/u/:id", userRoute);
 app.put("/user/:email", userRoute);
 
 /* async function run(){
